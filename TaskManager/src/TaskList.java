ff16ffa12491f585fa85d59543c263c3be8447d9
jshortz <jessica.shortz@gmail.com>
2019-10-08 22:06:26 +0200

    Moved TaskManager functionality to TaskList. Skeletal outline of all methods, a few implementations. Basic TaskList structure functions, no user input added

--- TaskManager/src/TaskList.java
+++ TaskManager/src/TaskList.java
@@ -1,20 +1,23 @@
 import java.util.ArrayList;
+import java.nio.file.*;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * This class is part of the "Task Manager" application.
  * "Task Manager" is a simple, consoled-based to-do list application.
  *
  * This class creates a list to hold Task objects and provides methods to make
- * alterations to the list
+ * alterations to the list, as well as methods to run the program and interact with the list.
  *
  * @author  Jessica Shortz
- * @version 2019.10.07
+ * @version 2019.10.08
  */
 
 public class TaskList
 {
     // an ArrayList to hold the list of Tasks
-    private ArrayList<Task> taskList;
+    protected static ArrayList<Task> taskList = new ArrayList<>();
 
     /**
      * Constructor - creates an empty list, taskList
@@ -26,39 +29,48 @@ public class TaskList
 
     /**
      * Add a task to the taskList
-     * @param Task to be added to the list
+     * @param taskToAdd Task to be added to the list
      */
-    public void addTaskToList(Task taskToAdd)
+    // This assumes that each task is given a variable name - if used as generics, change this
+    public static void addTaskToList(Task taskToAdd)
     {
         taskList.add(taskToAdd);
     }
 
     /**
      * Remove a selected task from the taskList by title
-     * @param title of task to be removed
+     * @param taskTitleToRemove title of task to be removed
      */
+    // Implementation of getting from user will need to include determining which task by variable name
     public void removeTaskByTitle(String taskTitleToRemove)
     {
         taskList.remove(taskTitleToRemove);
     }
 
+    /* CURRENTLY A REACH IMPLEMENTATION, NOT REQUIRED BY DOCUMENTATION
     /**
      * Remove all tasks associated with a particular project
-     * @param project title to be removed
-     */
+     * @param taskProjectToRemove project title to be removed
+
     public void removeTaskByProject(String taskProjectToRemove)
     {
         // Loop over list and collect all by project
+        // TODO: Use stream.filter to do this
         taskList.remove(taskProjectToRemove);
     }
+    */
 
     /**
      * Edit a selected task from the taskList
-     * @param Task to be edited
+     * @param taskToEdit Task to be edited
      */
-    public void editTask(Task taskToEdit)
+    public void editTask(Task taskToEdit, String newTitle, String project, Date newDueDate, boolean progress, boolean changeComplete)
     {
-
+        taskToEdit.title = newTitle;
+        taskToEdit.partOfProject = project;
+        taskToEdit.dueDate = newDueDate;
+        taskToEdit.inProgress = progress;
+        taskToEdit.complete = changeComplete;
     }
 
     /**
@@ -66,7 +78,7 @@ public class TaskList
      */
     public void sortListByTitle()
     {
-
+        // TODO: IMPLEMENT
     }
 
     /**
@@ -74,7 +86,7 @@ public class TaskList
      */
     public void sortListByDueDate()
     {
-
+        // TODO: IMPLEMENT
     }
 
     /**
@@ -82,18 +94,85 @@ public class TaskList
      */
     public void sortListByProject()
     {
-
+        // TODO: IMPLEMENT
     }
 
     /**
      * Check whether there are two tasks with the same title
-     * @param title to be checked
+     * @param titleToBeChecked title to be checked
      * @return Boolean (true if duplicate title, false otherwise)
      */
     public boolean isDuplicateTitle(String titleToBeChecked)
     {
+        //TODO: COMPLETE IMPLEMENTATION
+        return false;
+    }
+
+    public void saveAndQuit(String filename)
+    {
+        String path = filename;
+        String content = taskList.toString();
+        try {
+            Files.writeString(Paths.get(path), content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Display the  contents of the user manual
+     * @param filename The filename containing the user manual
+     * @return A String containing the contents of the user manual file
+     */
+
+    public static void displayHelp(String filename)
+    {
 
     }
 
+    /**
+     * Display the  contents of the chosen TaskList
+     * @param tasklist The TaskList to be displayed
+     * @return A String containing the contents of the chosen TaskList
+     */
+
+    public static void displayTaskList()
+    {   System.out.println();
+        System.out.println(taskList.get(0).toString());
+        System.out.println(taskList.get(1).toString());
+    }
+
+    /**
+     * Load the contents of the tasklist saved file to be the current tasklist
+     * @param filename The file to be used to populate the list
+     */
+
+    public void loadTaskList(String filename)
+    {
+
+    }
+
+    /**
+     * Display a welcome message to the user
+     */
+
+    public static void printWelcome()
+    {
+        System.out.println("Welcome to Task Manager, a program to manage a to-do list!");
+    }
+
+    /**
+     * Main method used to run core functionalities of system
+     */
+
+    public static void main(String[] args) {
+        Date current = new Date();
+        Task dishes = new Task("Wash the dishes", current, "Household");
+        addTaskToList(dishes);
+        Task laundry = new Task("Put in machine", current, "Household");
+        addTaskToList(laundry);
+        printWelcome();
+        displayTaskList();
+    }
 
 }
--- TaskManager/src/TaskManager.java
+++ TaskManager/src/TaskManager.java
@@ -1,3 +1,5 @@
+
+
 /**
  * This class is part of the "Task Manager" application.
  * "Task Manager" is a simple, consoled-based to-do list application.
@@ -6,46 +8,53 @@
  * the main method.
  *
  * @author  Jessica Shortz
- * @version 2019.10.07
+ * @version 2019.10.08
  */
 
 public class TaskManager
 {
+
     /**
      * Save the to-do list to an external file and quit the application
-     * @param The filename for the list to be saved to
+     * @param filename The filename for the list to be saved to
      */
 
     public void saveAndQuit(String filename)
     {
-
+        String path = filename;
+        String content = taskList.toString();
+        try {
+            Files.writeString(Paths.get(path), content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Display the  contents of the user manual
-     * @param The filename containing the user manual
+     * @param filename The filename containing the user manual
      * @return A String containing the contents of the user manual file
      */
 
-    public String displayHelp(String filename)
+    public static void displayHelp(String filename)
     {
 
     }
 
     /**
      * Display the  contents of the chosen TaskList
-     * @param The TaskList to be displayed
+     * @param tasklist The TaskList to be displayed
      * @return A String containing the contents of the chosen TaskList
      */
 
-    public String displayTaskList(String tasklist)
+    public static void displayTaskList()
     {
-
+        System.out.println(taskList.toString());
     }
 
     /**
      * Load the contents of the tasklist saved file to be the current tasklist
-     * @param The file to be used to populate the list
+     * @param filename The file to be used to populate the list
      */
 
     public void loadTaskList(String filename)
@@ -57,9 +66,9 @@ public class TaskManager
      * Display a welcome message to the user
      */
 
-    public void printWelcome()
+    public static void printWelcome()
     {
-
+        System.out.println("Welcome to Task Manager, a program to manage a to-do list!");
     }
 
     /**
@@ -67,6 +76,7 @@ public class TaskManager
      */
 
     public static void main(String[] args) {
-
+        printWelcome();
+        displayTaskList();
     }
 }
